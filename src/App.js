a83115b181b29ca98b8bf6292336ac14887cde09
MdRiabi <mhdriabi2013@gmail.com>
2024-04-21 01:41:05 +0200

    orgnize component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import Counter from './Components/Counter';
-import Counter1 from './Components/Counter1';
+import Counter from './Components/UseState/Counter';
+import Counter1 from './Components/UseState/Counter1';
+import Counter2 from './Components/UseState/Counter2';
+import HookStateArray from './Components/UseState/HookStateArray';
 
 function App() {
   return (
     <div>
        {/* <Counter /> */}
        <Counter1 />
+       <Counter/>
+       <Counter2/>
+       <HookStateArray/>
     </div>
    
   );
