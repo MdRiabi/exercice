4288ea35e3249637d3a51789aa2de1a6fa29378d
MdRiabi <mhdriabi2013@gmail.com>
2024-04-24 00:03:58 +0200

    working with useReducer Hook

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import HookStateArray from "./Components/UseState/HookStateArray";
 import EffectExample1 from "./Components/UseEffectPractice/EffectExample1";
 import ComponentA from "./Components/Context/ComponentA";
 import React from "react";
+import MyCounter1 from "./Components/useReducerPractice/MyCounter1";
 
 export const PriceContext = React.createContext();
 export const ItemContext = React.createContext();
 function App() {
+
   return (
     <div>
       {/* <Counter /> */}
@@ -20,11 +22,16 @@ function App() {
        <Counter2/>
        <HookStateArray/>
        <EffectExample1/> */}
-      <PriceContext.Provider value={"$522"}>
+      {/* <PriceContext.Provider value={"$522"}>
         <ItemContext.Provider value={"Nokia"}>
-          <ComponentA />
+
+        <ComponentA />
+
         </ItemContext.Provider>
-      </PriceContext.Provider>
+        
+      </PriceContext.Provider> */}
+
+      <MyCounter1/>
     </div>
   );
 }
