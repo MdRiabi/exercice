57b34bc8af84192af400adacd4ca84ec9faad0e1
MdRiabi <mhdriabi2013@gmail.com>
2024-04-23 19:33:03 +0200

    working with fake API

---
+++ src/Components/DataFetching/FetchMultiplePostes.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+function FetchMultiplePostes() {
+    const [postes, setPostes] = useState([])
+    useEffect(() =>{
+     axios.get('https://jsonplaceholder.typicode.com/posts')
+     .then((result) => {
+        console.log(result)
+        setPostes(result.data);
+     }).catch((err) => {
+        console.log(err)
+        
+     });
+    })
+    return (
+        <div>
+            <ul>
+                {postes.map(post =>
+                    <li key = {post.id}>{post.title}</li>
+                )}
+            </ul>
+        </div>
+        
+    )
+}
+
+export default FetchMultiplePostes
