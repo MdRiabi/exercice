2430e42ed83f0a254d517396c85604bd758b29ee
MdRiabi <mhdriabi2013@gmail.com>
2024-04-24 14:31:59 +0200

    use useContext with useReducer To Render the prop in the child components

--- src/App.js
+++ src/App.js
@@ -5,41 +5,83 @@ import Counter1 from "./Components/UseState/Counter1";
 import Counter2 from "./Components/UseState/Counter2";
 import HookStateArray from "./Components/UseState/HookStateArray";
 import EffectExample1 from "./Components/UseEffectPractice/EffectExample1";
-import ComponentA from "./Components/Context/ComponentA";
-import React from "react";
+/* import ComponentA from "./Components/Context/ComponentA"; */
+import ComponentA from "./Components/useContextWithReducer/ComponentA";
+import ComponentX from "./Components/useContextWithReducer/ComponentX";
+import ComponentZ from "./Components/useContextWithReducer/ComponentZ";
+import React, { useReducer } from "react";
 import MyCounter1 from "./Components/useReducerPractice/MyCounter1";
 import MyCounter2 from "./Components/useReducerPractice/MyCounter2";
 import MyCounter3 from "./Components/useReducerPractice/MyCounter3";
 export const PriceContext = React.createContext();
 export const ItemContext = React.createContext();
+
+export const CountContext = React.createContext();
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <div>
-      {/* <Counter /> */}
-
-      {/* <Counter1 />
-       <Counter/>
-       <Counter2/>
-       <HookStateArray/>
-       <EffectExample1/> */}
-      {/* <PriceContext.Provider value={"$522"}>
-        <ItemContext.Provider value={"Nokia"}>
-
+    <CountContext.Provider value={{CoutState: count, CountDispatch: dispatch}}>
+      <div className="App">
+        Count: {count}
         <ComponentA />
-
-        </ItemContext.Provider>
-        
-      </PriceContext.Provider> */}
-
-      {/* <MyCounter1/> */}
-      {/* <MyCounter2 /> */}
-      <MyCounter3/>
-    </div>
+        <ComponentX />
+        <ComponentZ />
+      </div>
+    </CountContext.Provider>
   );
 }
 
 export default App;
 
 /* object as state variable
+
  */
+
+/* <div>*/
+{
+  /* <Counter /> */
+}
+
+{
+  /* <Counter1 />
+       <Counter/>
+       <Counter2/>
+       <HookStateArray/>
+       <EffectExample1/> */
+}
+{
+  /* <PriceContext.Provider value={"$522"}>
+        <ItemContext.Provider value={"Nokia"}>
+
+        <ComponentA />
+
+        </ItemContext.Provider>
+        
+      </PriceContext.Provider> */
+}
+
+{
+  /* <MyCounter1/> */
+}
+{
+  /* <MyCounter2 /> */
+}
+{
+  /* <MyCounter3/> */
+}
+/* </div> */
---
+++ src/Components/useContextWithReducer/ComponentZ.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import { CountContext } from "../../App";
+
+function ComponentZ() {
+  const countContext = useContext(CountContext);
+
+  return (
+    <div>
+      <h1>Count From Component Z: {countContext.CoutState}</h1>
+      <button onClick={() => countContext.CountDispatch("increment")}>increment</button>
+      <button onClick={() => countContext.CountDispatch("decrement")}>decrement</button>
+      <button onClick={() => countContext.CountDispatch("reset")}>reset</button>
+    </div>
+  );
+}
+
+export default ComponentZ;
